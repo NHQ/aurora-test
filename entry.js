802ed816c1c46f785c1180a4e13f721632999bd0
johnnyscript <mostmodernist@gmail.com>
2014-10-19 11:28:21 -0700

    changed url

--- entry.js
+++ entry.js
@@ -9,7 +9,6 @@ var sampleRate = 44100//evt.data.sampleRate;
 var resamplers = [];
 var chans = 1;
 var player = av.Asset.fromURL('/aurora-test/audio.mp3')
-//var player = av.Asset.fromURL('http://localhost:11002/youtube.mp3?https://www.youtube.com/watch?v=GUcXI2BIUOQ&list=RDGUcXI2BIUOQ&index=1')
 
 player.on('format', function(d){
 
@@ -17,10 +16,6 @@ player.on('format', function(d){
     chans = d.channelsPerFrame;
 
     var bufferSize = Math.ceil(4096 / (sampleRate / d.sampleRate));
-
-//    for(var x = 0; x < chans; x++){
-//      resamplers[x] = new Resampler(d.sampleRate, sampleRate, 1, bufferSize);
-//    }
 
   });
 
@@ -34,7 +29,6 @@ player.on('format', function(d){
 
   player.on('data', function(bufArray){
     for(x in resamplers){
-  //    bufArray[x] = resamplers[x].resampler(bufArray[x])
     }
     console.log(bufArray)
   });
